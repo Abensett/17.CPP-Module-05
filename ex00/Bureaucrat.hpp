09973c6f626c90a14bc7a7504e2e63c09c19e96c
Abensett <bensettiaimen@mailo.com>
2022-07-15 15:23:12 +0200

    go

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -30,7 +30,27 @@ class Bureaucrat
 
 		string 	getName(void) const;
 		int 	getGrade(void) const;
-	protected:
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw()
+				{
+					return ("Bureaucrat::exception : Grade is too high");
+				}
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw()
+				{
+					return ("Bureaucrat::exception : Grade is too low");
+				}
+		};
+
+
+	private:
 		string 	_name;
 		int		_grade;
 };
